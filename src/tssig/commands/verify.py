28edb9ed4eef5be96182a8ae92df99a04712b602
Daniel Hardman <daniel.hardman@gmail.com>
2025-03-05 18:33:06 -0700

    allow omission of .sig file in default case
    Signed-off-by: Daniel Hardman <daniel.hardman@gmail.com>

--- src/tssig/commands/verify.py
+++ src/tssig/commands/verify.py
@@ -3,7 +3,9 @@ from ..util import verify_by_pubkey
 
 def handler(args):
     """Verify a signature on a file."""
-    verify_by_pubkey(args.file, args.key, args.sig)
+    if not args.sig: args.sig = args.file + ".sig"
+    if verify_by_pubkey(args.file, args.key, args.sig):
+        print("Signature is valid.")
 
 parser = argparse.ArgumentParser(
     description='Verify the signature on a file.',
@@ -11,5 +13,5 @@ parser = argparse.ArgumentParser(
 )
 parser.add_argument("--key", "-k", required=True, help="privkey")
 parser.add_argument("--file", "-f", required=True, help="filename")
-parser.add_argument("--sig", "-s", required=True, help="signature")
+parser.add_argument("--sig", "-s", help="signature")
 parser.set_defaults(handler=handler)
